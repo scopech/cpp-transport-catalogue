bb4651ada2cf164a0a741c323d08119eb028d3df
scopech <scopech@yandex.ru>
2026-01-24 20:31:29 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,1 +1,62 @@
-// место для вашего кода
+#pragma once
+
+#include <deque>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <string_view>
+#include <optional>
+
+#include "geo.h"
+
+namespace transport {
+
+namespace domain {
+
+struct Stop {
+    std::string name;
+    geo::Coordinates coordinates;
+};
+
+struct Bus {
+    std::string name;
+    std::vector<const Stop*> stops;
+};
+
+struct BusStat {
+    size_t stops_count;
+    size_t unique_stops_count;
+    double route_length;
+};
+
+struct StopInfo {
+    const std::set<std::string_view>* buses;
+};
+
+} // namespace domain
+
+class TransportCatalogue {
+public:
+    void AddStop(std::string_view name, geo::Coordinates coordinates);
+    void AddBus(std::string_view name, const std::vector<std::string_view>& stop_names);
+
+    const domain::Bus* FindBus(std::string_view name) const;
+    const domain::Stop* FindStop(std::string_view name) const;
+
+    std::optional<domain::BusStat> GetBusInfo(std::string_view bus_name) const;
+    
+    std::optional<domain::StopInfo> GetStopInfo(std::string_view stop_name) const;
+
+private:
+    std::deque<domain::Stop> stops_;
+    std::deque<domain::Bus> buses_;
+
+    std::unordered_map<std::string_view, const domain::Stop*> stopname_to_stop_;
+    std::unordered_map<std::string_view, const domain::Bus*> busname_to_bus_;
+
+    std::unordered_map<const domain::Stop*, std::set<std::string_view>> stop_to_buses_;
+};
+
+} // namespace transport
