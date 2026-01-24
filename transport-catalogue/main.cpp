954b1c44949d447056ff5b8b362b3fc68c0f1d91
scopech <scopech@yandex.ru>
2026-01-24 20:29:38 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,35 @@
-// место для вашего кода
+#include <iostream>
+#include <string>
+
+#include "input_reader.h"
+#include "stat_reader.h"
+#include "transport_catalogue.h"
+
+using namespace std;
+
+using namespace transport;
+
+int main() {
+    TransportCatalogue catalogue;
+
+    int base_request_count;
+    cin >> base_request_count >> ws;
+
+    {
+        input::InputReader reader;
+        for (int i = 0; i < base_request_count; ++i) {
+            string line;
+            getline(cin, line);
+            reader.ParseLine(line);
+        }
+        reader.ApplyCommands(catalogue);
+    }
+
+    int stat_request_count;
+    cin >> stat_request_count >> ws;
+    for (int i = 0; i < stat_request_count; ++i) {
+        string line;
+        getline(cin, line);
+        stat::ParseAndPrintStat(catalogue, line, cout);
+    }
+}
