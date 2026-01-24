145b70228baf252a39f3e0908481cef385b6d4e2
scopech <scopech@yandex.ru>
2026-01-24 20:30:17 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,1 +1,61 @@
-// место для вашего кода
+#include "stat_reader.h"
+#include <iostream>
+#include <iomanip>
+
+namespace transport::stat {
+
+namespace detail {
+    
+    std::string_view TrimRequest(std::string_view str) {
+        const auto start = str.find_first_not_of(' ');
+        if (start == str.npos) {
+            return {};
+        }
+        return str.substr(start, str.find_last_not_of(' ') + 1 - start);
+    }
+
+} // namespace detail
+
+void ParseAndPrintStat(const transport::TransportCatalogue& transport_catalogue, 
+                       std::string_view request,
+                       std::ostream& output) {
+    using namespace detail;
+
+    std::string_view command_bus = "Bus ";
+    std::string_view command_stop = "Stop ";
+
+    if (request.substr(0, command_bus.size()) == command_bus) {
+        std::string_view bus_name = TrimRequest(request.substr(command_bus.size()));
+        auto stat = transport_catalogue.GetBusInfo(bus_name);
+
+        if (stat) {
+            output << "Bus " << bus_name << ": " 
+                   << stat->stops_count << " stops on route, " 
+                   << stat->unique_stops_count << " unique stops, " 
+                   << std::setprecision(6) << stat->route_length << " route length" << std::endl;
+        } else {
+            output << "Bus " << bus_name << ": not found" << std::endl;
+        }
+
+    } else if (request.substr(0, command_stop.size()) == command_stop) {
+        std::string_view stop_name = TrimRequest(request.substr(command_stop.size()));
+        auto stat = transport_catalogue.GetStopInfo(stop_name);
+
+        if (stat) {
+            const auto& buses = *stat->buses;
+            if (buses.empty()) {
+                output << "Stop " << stop_name << ": no buses" << std::endl;
+            } else {
+                output << "Stop " << stop_name << ": buses";
+                for (std::string_view bus : buses) {
+                    output << " " << bus;
+                }
+                output << std::endl;
+            }
+        } else {
+            output << "Stop " << stop_name << ": not found" << std::endl;
+        }
+    }
+}
+
+} // namespace transport::stat
