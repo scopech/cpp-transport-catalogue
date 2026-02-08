9261e0bfad9d21e8a73c73ccd607d725bdc4e3f9
scopech <scopech@yandex.ru>
2026-02-08 04:14:29 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -8,6 +8,7 @@
 #include <set>
 #include <string_view>
 #include <optional>
+#include <utility>
 
 #include "geo.h"
 
@@ -29,6 +30,7 @@ struct BusStat {
     size_t stops_count;
     size_t unique_stops_count;
     double route_length;
+    double curvature;
 };
 
 struct StopInfo {
@@ -37,10 +39,21 @@ struct StopInfo {
 
 } // namespace domain
 
+struct PairHasher {
+    size_t operator()(const std::pair<const domain::Stop*, const domain::Stop*>& pair) const {
+        auto h1 = std::hash<const void*>{}(pair.first);
+        auto h2 = std::hash<const void*>{}(pair.second);
+        return h1 ^ (h2 << 1);
+    }
+};
+
 class TransportCatalogue {
 public:
     void AddStop(std::string_view name, geo::Coordinates coordinates);
     void AddBus(std::string_view name, const std::vector<std::string_view>& stop_names);
+    
+    void SetDistance(const domain::Stop* from, const domain::Stop* to, double distance);
+    double GetDistance(const domain::Stop* from, const domain::Stop* to) const;
 
     const domain::Bus* FindBus(std::string_view name) const;
     const domain::Stop* FindStop(std::string_view name) const;
@@ -57,6 +70,7 @@ private:
     std::unordered_map<std::string_view, const domain::Bus*> busname_to_bus_;
 
     std::unordered_map<const domain::Stop*, std::set<std::string_view>> stop_to_buses_;
+    std::unordered_map<std::pair<const domain::Stop*, const domain::Stop*>, double, PairHasher> distances_;
 };
 
 } // namespace transport
