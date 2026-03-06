9952857d01b210f7e7a11d1d610b165aeadec99d
Egorov Dmitry <scopech@yandex.ru>
2026-03-06 23:36:51 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,35 +1,27 @@
-#include <iostream>
-#include <string>
-
-#include "input_reader.h"
-#include "stat_reader.h"
+#include "json_reader.h"
 #include "transport_catalogue.h"
+#include "request_handler.h"
+#include "map_renderer.h"
+
+#include <iostream>
 
 using namespace std;
 
-using namespace transport;
-
 int main() {
-    TransportCatalogue catalogue;
-
-    int base_request_count;
-    cin >> base_request_count >> ws;
-
-    {
-        input::InputReader reader;
-        for (int i = 0; i < base_request_count; ++i) {
-            string line;
-            getline(cin, line);
-            reader.ParseLine(line);
-        }
-        reader.ApplyCommands(catalogue);
+    transport::TransportCatalogue catalogue;
+    JsonReader json_reader(cin);
+    
+    json_reader.ProcessBaseRequests(catalogue);
+    
+    renderer::RenderSettings render_settings;
+    if (json_reader.HasRenderSettings()) {
+        render_settings = json_reader.ParseRenderSettings();
     }
-
-    int stat_request_count;
-    cin >> stat_request_count >> ws;
-    for (int i = 0; i < stat_request_count; ++i) {
-        string line;
-        getline(cin, line);
-        transport::stat::ParseAndPrintStat(catalogue, line, cout);
-    }
+    
+    renderer::MapRenderer renderer(render_settings);
+    RequestHandler handler(catalogue, renderer);
+    
+    json_reader.ProcessStatRequests(handler, cout);
+    
+    return 0;
 }
