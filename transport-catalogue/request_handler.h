60dc7e828b63f329dced05739a86be53790e3aab
Egorov Dmitry <scopech@yandex.ru>
2026-04-02 21:28:20 +0300

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -2,19 +2,22 @@
 
 #include "transport_catalogue.h"
 #include "map_renderer.h"
+#include "transport_router.h"
 #include "svg.h"
 #include <optional>
 #include <string_view>
 
 class RequestHandler {
 public:
-    RequestHandler(const transport::TransportCatalogue& db, const renderer::MapRenderer& renderer);
+    RequestHandler(const transport::TransportCatalogue& db, const renderer::MapRenderer& renderer, const transport::TransportRouter& router);
 
     std::optional<transport::domain::BusStat> GetBusStat(const std::string_view& bus_name) const;
     std::optional<transport::domain::StopInfo> GetBusesByStop(const std::string_view& stop_name) const;
+    std::optional<transport::RouteInfo> GetRoute(const std::string_view& from, const std::string_view& to) const;
     svg::Document RenderMap() const;
 
 private:
     const transport::TransportCatalogue& db_;
     const renderer::MapRenderer& renderer_;
+    const transport::TransportRouter& router_;
 };
